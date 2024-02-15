fc926aeb19bf865438dffb384ea27ffc82a2af33
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-02-15 15:04:33 +0800

    Rmv JS

--- script.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello, World!');
-const body = document.querySelector('body');
-const smallIcon = document.createElement('img');
-
-smallIcon.src = './img/odin-logo.svg';
-
-body.appendChild(smallIcon);
